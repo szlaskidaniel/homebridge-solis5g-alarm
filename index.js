fb37745ff6e1ba3dc269efc3c0d62cd49a5ee7ad
Daniel Szlaski <daniel.szlaski@genesys.com>
2022-08-07 17:42:56 +0200

    fix auth issue

--- index.js
+++ index.js
@@ -41,13 +41,14 @@ Solis5G.prototype = {
 
   _httpRequest: function (url, username, password, headers, callback) { 
     this.log.debug('_httpRequest', url);
+    const encodedAuth = Base64.encode(`${username}:${password}`);
+    
     request({
       url: url,      
-      method: 'GET',
-      auth: {
-        user: username,
-        password: password
-      }      
+      method: 'GET',      
+      headers: {
+        'Authorization': 'Basic ' + encodedAuth
+      }   
     },
     function (error, response, body) {
       callback(error, response, body)
@@ -81,13 +82,14 @@ Solis5G.prototype = {
           //this.log.debug('Device response: %s', responseBody);
 
           this.log.debug('looking for var webdata_alarm = ""');
-          const bAlarmFound = responseBody.indexOf('var webdata_alarm = ""');
+          const bNoAlarmFound = responseBody.indexOf('var webdata_alarm = ""');
           
-          if (bAlarmFound > 0) {
-            this.log.warn('Device in alarm mode');
+          
+          if (bNoAlarmFound > 0) {                        
+            this.log.warn('Device ok. No alarm found');
             this.service.getCharacteristic(Characteristic.On).updateValue(0);
           } else { 
-            this.log.debug('Alarm found');
+            this.log.warn('Device in alarm mode');
             this.service.getCharacteristic(Characteristic.On).updateValue(1);
           }
                 
@@ -118,4 +120,131 @@ Solis5G.prototype = {
     return [this.informationService, this.service]
   }
 
+}
+
+var Base64 = {
+
+  // private property
+  _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+  // public method for encoding
+  encode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = Base64._utf8_encode(input);
+
+      while (i < input.length) {
+
+          chr1 = input.charCodeAt(i++);
+          chr2 = input.charCodeAt(i++);
+          chr3 = input.charCodeAt(i++);
+
+          enc1 = chr1 >> 2;
+          enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+          enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+          enc4 = chr3 & 63;
+
+          if (isNaN(chr2)) {
+              enc3 = enc4 = 64;
+          } else if (isNaN(chr3)) {
+              enc4 = 64;
+          }
+
+          output = output +
+          this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+          this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+      }
+      return output;
+  },
+
+  // public method for decoding
+  decode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+      while (i < input.length) {
+
+          enc1 = this._keyStr.indexOf(input.charAt(i++));
+          enc2 = this._keyStr.indexOf(input.charAt(i++));
+          enc3 = this._keyStr.indexOf(input.charAt(i++));
+          enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+
+          output = output + String.fromCharCode(chr1);
+
+          if (enc3 != 64) {
+              output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+              output = output + String.fromCharCode(chr3);
+          }
+      }
+
+      output = Base64._utf8_decode(output);
+
+      return output;
+  },
+
+  // private method for UTF-8 encoding
+  _utf8_encode : function (string) {
+      string = string.replace(/\r\n/g,"\n");
+      var utftext = "";
+
+      for (var n = 0; n < string.length; n++) {
+
+          var c = string.charCodeAt(n);
+
+          if (c < 128) {
+              utftext += String.fromCharCode(c);
+          }
+          else if((c > 127) && (c < 2048)) {
+              utftext += String.fromCharCode((c >> 6) | 192);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+          else {
+              utftext += String.fromCharCode((c >> 12) | 224);
+              utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+      }
+      return utftext;
+  },
+
+  // private method for UTF-8 decoding
+  _utf8_decode : function (utftext) {
+      var string = "";
+      var i = 0;
+      var c = c1 = c2 = 0;
+
+      while ( i < utftext.length ) {
+
+          c = utftext.charCodeAt(i);
+
+          if (c < 128) {
+              string += String.fromCharCode(c);
+              i++;
+          }
+          else if((c > 191) && (c < 224)) {
+              c2 = utftext.charCodeAt(i+1);
+              string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+              i += 2;
+          }
+          else {
+              c2 = utftext.charCodeAt(i+1);
+              c3 = utftext.charCodeAt(i+2);
+              string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+              i += 3;
+          }
+      }
+      return string;
+  }
 }
